fcd7177967844a4f491ba3d19a5d49dbd0d93f18
david reinfurt <reinfurt@Adriano-Olivetti.local>
2017-03-25 09:35:49 +0000

    update analyzer.js

--- static/js/analyzer.js
+++ static/js/analyzer.js
@@ -1,10 +1,11 @@
 // based on http://jsfiddle.net/pc76H/2/
+// requires <div id = 'anayzer'> as a container
+// *todo* pass url as a parameter to init()
 
 (function(){    
     var canvas, audio, ajax, source, analyser, sound, animation, w, h, context, button;
     var globalbuffer;
     var url = 'media/mp3/all-collected-voices_v1.mp3';
-    var bufferLoader;   // may be unnec.
     
     function init(){
         // canvas
@@ -13,7 +14,6 @@
         w=canvas.width=FF/2;
         h=canvas.height=200;
         context=canvas.getContext("2d");
-        // document.body.appendChild(canvas);
         var container = document.getElementById("analyzer");
         container.appendChild(canvas);
 
@@ -28,26 +28,13 @@
         analyser.connect(audio.destination);
 
         // document
-        // button = document.createElement('button');
-        // button.addEventListener('click', start, false);
-        // document.body.appendChild(button);
         document.addEventListener('click', start, false);
 
         // load file
         requeststream(url);
-        // loadbuffer();
         window.removeEventListener('load',init,false);
     }
 
-    function progress(e){
-        context.fillStyle='hsla('+(e.loaded/e.total*240)+',100%,50%,1)';
-        context.fillRect(0,1,e.loaded/e.total*w,1);
-    }
-/*
-    function buffer(){
-        audio.decodeAudioData(this.response,start,function(){console.log('Decoding Error')});
-    }
-*/
     function animate(){
         var a = new Uint8Array(analyser.frequencyBinCount);
         var y = new Uint8Array(analyser.frequencyBinCount);
@@ -59,15 +46,13 @@
         context.clearRect(0, 0, w, h);
         while(b--){
             var bh=a[b]+1;
-            // context.fillStyle='hsla('+(b/c*240)+','+(y[b]/255*100|0)+'%,50%,1)';
-            context.fillStyle='#000000';
+            // context.fillStyle='hsla('+(b/c*240)+','+(y[b]/255*100|0)+'%,50%,1)';     // rainbow
+            context.fillStyle='#000000';        // black
             // context.fillRect(1*b,h-bh,1,bh); // spectrum
             context.fillRect(1*b,y[b],1,1);     // wave
         }
         window.requestAnimationFrame = window.requestAnimationFrame || window.$
         animation=requestAnimationFrame(animate);
-
-        // animation=webkitRequestAnimationFrame(animate);
     }
 
     function readfile(e){
@@ -83,30 +68,15 @@
         // source.noteOn(0);    // deprecated, use start();
         source.start(0);
         window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
-        animation=requestAnimationFrame(animate);
+        animation = requestAnimationFrame(animate);
     }
-
 
     // new
 
-    function requestfile(thisurl){
-        var request = new XMLHttpRequest();
-        request.open('GET', thisurl, true);
-        // request.responseType = 'arraybuffer';    // problem to do w/responsetype
-        request.addEventListener("load", requestfilelistener);
-        /* 
-        // not working, to do with loading binary data
-        request.onload = function(e) {
-            audio.decodeAudioData(request.result,start,function(){console.log('Decoding Error')});
-        }
-        */
-        request.send();
-    }
-
     function requeststream(thisurl){
         var request = new XMLHttpRequest();
-        // request.addEventListener("load", requestfilelistener);
-        request.open('GET', thisurl, true);
+        request.addEventListener("load", requestfilelistener);
+        request.open('GET', thisurl, true);     // async = true
         request.responseType = 'arraybuffer';
         // decode async
         request.onload = function() {           
@@ -114,40 +84,20 @@
                 function(buffer) {
                     globalbuffer = buffer;
                     console.log(globalbuffer);
+                    start();        // does this need the event passed to it?
+                                    // this will only work on desktop
+                                    // *fix* try catch fail ios
                 }, function(){
-                    console.log('Decoding Error')
+                    console.log('Decoding error . . .')
                 });
         }
         request.send();
     }
 
     function requestfilelistener () {
-        console.log(this.responseText);
+        console.log(this);
+        console.log(this.statusText);
     }
     
-
-
-// not working, as does not find BufferLoader (prob mozilla specific)
-function loadbuffer() {
-  bufferLoader = new BufferLoader(audio, ['portauthority.mp3','portauthority.mp3'], finishedLoading);
-  bufferLoader.load();
-}
-
-function finishedLoading(bufferList) {
-  // create two sources and play them both together.
-  var source1 = context.createBufferSource();
-  var source2 = context.createBufferSource();
-  source1.buffer = bufferList[0];
-  source2.buffer = bufferList[1];
-
-  source1.connect(context.destination);
-  source2.connect(context.destination);
-  source1.start(0);
-  source2.start(0);
-}
-
-
-    
-
     window.addEventListener('load',init,false);
 })();
